3685ba5e697264f6001435290899b16629e14857
Calvin Metcalf <cmetcalf@appgeo.com>
2014-12-22 13:59:03 -0500

    pull out rsa stuff (adds blinding)

--- privateDecrypt.js
+++ privateDecrypt.js
@@ -2,7 +2,7 @@ var parseKeys = require('parse-asn1');
 var mgf = require('./mgf');
 var xor = require('./xor');
 var bn = require('bn.js');
-
+var crt = require('browserify-rsa');
 module.exports = function (crypto) {
   return privateDecrypt;
   function privateDecrypt(private_key, enc) {
@@ -33,57 +33,7 @@ module.exports = function (crypto) {
     }
   }
 };
-function crt(msg, priv, crypto) {
-  var blinds = blind(priv, crypto);
-  var mod = bn.mont(priv.modulus);
-  var blinded = new bn(msg)
-  .toRed(mod)
-  .redIMul(blinds.blinder.toRed(mod))
-  .fromRed();
-  var c1 = blinded.toRed(bn.mont(priv.prime1));
-  var c2 = blinded.toRed(bn.mont(priv.prime2));
-  var qinv = priv.coefficient;
-  var p = priv.prime1;
-  var q = priv.prime2;
-  var m1 = c1.redPow(priv.exponent1);
-  var m2 = c2.redPow(priv.exponent2);
-  m1 = m1.fromRed();
-  m2 = m2.fromRed();
-  var h = m1.isub(m2).imul(qinv).mod(p);
-  h.imul(q);
-  m2.iadd(h);
-  return new Buffer(m2.toRed(mod).redIMul(blinds.unblinder.toRed(mod)).fromRed().toArray());
-}
-// based on https://github.com/google/end-to-end/blob/bd14d9607e742cd94b1a5af39e0f9e8c454b4a32/src/javascript/crypto/e2e/asymmetric/rsa.js#L196
-function blind(priv, crypto) {
-  var mod = bn.mont(priv.modulus);
-  var r = getr(priv, crypto);
-  var p = priv.prime1;
-  var q = priv.prime2;
-  var ONE = new bn(1);
-  var phi = ONE.toRed(mod)
-    .redSub(p.toRed(mod))
-    .redISub(q.toRed(mod))
-    .fromRed()
-    .isub(ONE);
-  var blinder = r.toRed(mod)
-  .redPow(phi)
-  .redPow(new bn(priv.publicExponent)).fromRed();
-  _blinder = blinder;
-  _unblinder = r;
-  return {
-    blinder: blinder,
-    unblinder:r
-  };
-}
-function getr(priv, crypto) {
-  var len = priv.modulus.byteLength();
-  var r = new bn(crypto.randomBytes(len));
-  while (r.cmp(priv.modulus) >= 0) {
-    r = new bn(crypto.randomBytes(len));
-  }
-  return r;
-}
+
 function oaep(key, msg, crypto){
   var n = key.modulus;
   var k = key.modulus.byteLength();
