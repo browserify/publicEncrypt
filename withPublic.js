9187d87c875788e3b6e72b71de68423fba5d997f
Calvin Metcalf <cmetcalf@appgeo.com>
2018-08-29 09:01:31 -0400

    switch to standard

--- withPublic.js
+++ withPublic.js
@@ -1,12 +1,12 @@
-var bn = require('bn.js');
-var Buffer = require('safe-buffer').Buffer;
+var BN = require('bn.js')
+var Buffer = require('safe-buffer').Buffer
 
-function withPublic(paddedMsg, key) {
+function withPublic (paddedMsg, key) {
   return Buffer.from(paddedMsg
-    .toRed(bn.mont(key.modulus))
-    .redPow(new bn(key.publicExponent))
+    .toRed(BN.mont(key.modulus))
+    .redPow(new BN(key.publicExponent))
     .fromRed()
-    .toArray());
+    .toArray())
 }
 
-module.exports = withPublic;
+module.exports = withPublic
